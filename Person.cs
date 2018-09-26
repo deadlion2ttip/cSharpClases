dd30eb458f1ed618accf96206ce5fb5f72591dfe
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-26 13:17:31 -0400

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is {0} {1}", FirstName, LastName);
+        }
+
+        public void Threaten()
+        {
+            Console.WriteLine("You Killed My Father, Prepare to Die");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Chris = new Person();
+            Chris.FirstName = "Chris";
+            Chris.LastName = "Griffin";
+
+            Chris.Introduce();
+            Chris.Threaten();
+        }
+    }
+}
